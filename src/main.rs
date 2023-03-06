cba636194d13bc3b4b17c5b209c4801ddd5e6a9a
wholteza <zackarias@montell.se>
2023-03-06 17:36:28 +0100

    Solved issue of not being able to match passwords

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::array;
 use std::fmt::Error;
 use std::fs::{self, DirEntry};
 use std::io::{self, stdin};
-use std::ops::Deref;
 use std::path::Path;
+use std::process::exit;
 
 fn parse_dir_entry(entry: &DirEntry) -> Result<Password, Error> {
     let name: String = match entry.file_name().to_str() {
@@ -25,8 +24,8 @@ fn parse_dir_entry(entry: &DirEntry) -> Result<Password, Error> {
 
     let password = Password {
         name,
-        absolute_path,
-        _relative_path: relative_path,
+        _absolute_path: absolute_path,
+        relative_path,
     };
 
     return Ok(password);
@@ -34,13 +33,34 @@ fn parse_dir_entry(entry: &DirEntry) -> Result<Password, Error> {
 
 fn extract_passwords(dir: &Path, passwords: &mut Vec<Password>) -> io::Result<()> {
     if dir.is_dir() {
+        let name = match dir.file_name() {
+            Some(file_name) => match file_name.to_str() {
+                Some(name) => name,
+                None => "",
+            },
+            None => "",
+        };
+
+        if name.contains(".git") {
+            return Ok(());
+        }
+
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
                 extract_passwords(&path, passwords)?;
             } else {
-                passwords.extend([parse_dir_entry(&entry).expect("Failed to parse password")]);
+                let name = match path.file_name() {
+                    Some(os_string_name) => match os_string_name.to_str() {
+                        Some(name) => name,
+                        None => "",
+                    },
+                    None => "",
+                };
+                if !name.contains(".gpg-id") {
+                    passwords.extend([parse_dir_entry(&entry).expect("Failed to parse password")]);
+                }
             }
         }
     }
@@ -50,46 +70,24 @@ fn extract_passwords(dir: &Path, passwords: &mut Vec<Password>) -> io::Result<()
 #[derive(Clone)]
 struct Password {
     name: String,
-    _relative_path: String,
-    absolute_path: String,
+    relative_path: String,
+    _absolute_path: String,
 }
 const DEFAULT_PATH: &str = "C:\\Users\\wholteza\\password-store";
 fn main() {
-    // get dir
-
-    println!("What is your pass root directory?");
-
-    let mut buffer: String = String::new();
-
-    _ = stdin().read_line(&mut buffer);
-    let directory_path: &str = match buffer.trim_end() {
-        "" => DEFAULT_PATH.into(),
-        name => name.into(),
-    };
-    println!("{directory_path}");
-
     // find all files
-    let path = Path::new(directory_path);
+    let path = Path::new(DEFAULT_PATH);
     if !path.is_dir() {
         println!("That is not a directory");
         return;
     }
     println!("That is a directory!");
 
-    let mut passwords: Vec<Password> = vec![];
+    let mut passwords = vec![];
     _ = extract_passwords(path, &mut passwords);
-
-    // print to screen
-    // for password in passwords {
-    //     println!(
-    //         "{}, {}, {}",
-    //         password.name, password.absolute_path, password.relative_path
-    //     );
-    // }
 
     passwords.iter().for_each(|p| println!("{}", p.name));
 
-    // get search string
     println!("What password are you looking for?");
     let mut search_input = String::new();
     stdin()
@@ -97,25 +95,19 @@ fn main() {
         .expect("A search string was not provided");
 
     // find file
-    // let found_password = passwords
-    //     .iter()
-    //     .find(|&p| (p.absolute_path.deref()).contains(&search_input));
+    let found_password = passwords
+        .iter()
+        .find(|&p| (p.relative_path.as_str()).contains(search_input.trim()));
 
-    for pass in passwords {
-        let b = search_input.to_ascii_uppercase();
-        let a = pass.name.to_ascii_uppercase();
-        println!("############################## {}, {}", a, b);
-        let is_match = a.find(&b);
-        if is_match.is_some() {
-            println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {}", a)
+    match found_password {
+        None => {
+            println!("Sorry but i cannot find that password");
+            exit(1)
+        }
+        Some(password) => {
+            println!("Found password: {}", password.relative_path)
         }
     }
 
-    // if found_password.is_none() {
-    //     println!("Sorry but i cannot find that password");
-    //     return;
-    // }
-
     // print file path or smth
-    // println!("Found password: {}", found_password.expect("never").name);
 }
